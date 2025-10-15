d96da14dd5e95033eb91972ce8564f0724cb7076
Marawan Shrief <147545917+Sharawelly@users.noreply.github.com>
2025-10-15 11:23:31 +0300

    Add files via upload

---
+++ week 12/Task 12 (linq)/Task 12 (linq)/Data/ApplicationDbContext.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Task_12.Models;
+
+namespace Task_12.Data;
+
+public partial class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext()
+    {
+    }
+
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Brand> Brands { get; set; }
+
+    public virtual DbSet<Category> Categories { get; set; }
+
+    public virtual DbSet<Customer> Customers { get; set; }
+
+    public virtual DbSet<Order> Orders { get; set; }
+
+    public virtual DbSet<OrderItem> OrderItems { get; set; }
+
+    public virtual DbSet<Product> Products { get; set; }
+
+    public virtual DbSet<Staff> Staffs { get; set; }
+
+    public virtual DbSet<Stock> Stocks { get; set; }
+
+    public virtual DbSet<Store> Stores { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=BikeStore;Integrated Security=True;Connect Timeout=30;Encrypt=True;Trust Server Certificate=True;Application Intent=ReadWrite;Multi Subnet Failover=False");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Brand>(entity =>
+        {
+            entity.HasKey(e => e.BrandId).HasName("PK__brands__5E5A8E2772D26FEE");
+        });
+
+        modelBuilder.Entity<Category>(entity =>
+        {
+            entity.HasKey(e => e.CategoryId).HasName("PK__categori__D54EE9B4D7D8E269");
+        });
+
+        modelBuilder.Entity<Customer>(entity =>
+        {
+            entity.HasKey(e => e.CustomerId).HasName("PK__customer__CD65CB853398DE56");
+        });
+
+        modelBuilder.Entity<Order>(entity =>
+        {
+            entity.HasKey(e => e.OrderId).HasName("PK__orders__46596229CBA50D25");
+
+            entity.HasOne(d => d.Customer).WithMany(p => p.Orders)
+                .OnDelete(DeleteBehavior.Cascade)
+                .HasConstraintName("FK__orders__customer__46E78A0C");
+
+            entity.HasOne(d => d.Staff).WithMany(p => p.Orders)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__orders__staff_id__48CFD27E");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.Orders).HasConstraintName("FK__orders__store_id__47DBAE45");
+        });
+
+        modelBuilder.Entity<OrderItem>(entity =>
+        {
+            entity.HasKey(e => new { e.OrderId, e.ItemId }).HasName("PK__order_it__837942D48C5B3E6E");
+
+            entity.HasOne(d => d.Order).WithMany(p => p.OrderItems).HasConstraintName("FK__order_ite__order__4CA06362");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.OrderItems).HasConstraintName("FK__order_ite__produ__4D94879B");
+        });
+
+        modelBuilder.Entity<Product>(entity =>
+        {
+            entity.HasKey(e => e.ProductId).HasName("PK__products__47027DF583DFA0CD");
+
+            entity.HasOne(d => d.Brand).WithMany(p => p.Products).HasConstraintName("FK__products__brand___3B75D760");
+
+            entity.HasOne(d => d.Category).WithMany(p => p.Products).HasConstraintName("FK__products__catego__3A81B327");
+        });
+
+        modelBuilder.Entity<Staff>(entity =>
+        {
+            entity.HasKey(e => e.StaffId).HasName("PK__staffs__1963DD9CCCBA4596");
+
+            entity.HasOne(d => d.Manager).WithMany(p => p.InverseManager).HasConstraintName("FK__staffs__manager___440B1D61");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.Staff).HasConstraintName("FK__staffs__store_id__4316F928");
+        });
+
+        modelBuilder.Entity<Stock>(entity =>
+        {
+            entity.HasKey(e => new { e.StoreId, e.ProductId }).HasName("PK__stocks__E68284D35B22522E");
+
+            entity.HasOne(d => d.Product).WithMany(p => p.Stocks).HasConstraintName("FK__stocks__product___5165187F");
+
+            entity.HasOne(d => d.Store).WithMany(p => p.Stocks).HasConstraintName("FK__stocks__store_id__5070F446");
+        });
+
+        modelBuilder.Entity<Store>(entity =>
+        {
+            entity.HasKey(e => e.StoreId).HasName("PK__stores__A2F2A30C5AE9BAD9");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ week 12/Task 12 (linq)/Task 12 (linq)/Program.cs
@@ -0,0 +1,302 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Task_12.Data;
+using Task_12.Models;
+
+namespace Task_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ApplicationDbContext _applicationDb = new ApplicationDbContext();
+
+            #region problem 1
+
+            //var customers = _applicationDb.Customers;
+
+            //// Table header
+            //Console.WriteLine("{0,-15} | {1,-15} | {2,-30}", "First Name", "Last Name", "Email");
+            //Console.WriteLine(new string('-', 65));
+
+            //// Table rows
+            //foreach (var customer in customers)
+            //{
+            //    Console.WriteLine("{0,-15} | {1,-15} | {2,-30}", customer.FirstName, customer.LastName, customer.Email);
+            //}
+
+            #endregion
+
+
+            #region problem 2
+
+            //var order = _applicationDb.Orders.FirstOrDefault(o => o.OrderId == 3);
+
+            //if (order != null)
+            //    Console.WriteLine($"Order ID: {order.OrderId}, Order Date: {order.OrderDate}, Customer ID: {order.CustomerId}, Staff ID: {order.StaffId}, Store ID: {order.StoreId}");
+
+
+            #endregion
+
+
+            #region problem 3
+            //var productsWithCategory = _applicationDb.Products
+            //    .Where(p => p.Category.CategoryName == "Mountain Bikes")
+            //    .Select(p => new
+            //    {
+            //        p.ProductId,
+            //        p.ProductName,
+
+            //    });
+
+            //foreach (var product in productsWithCategory)
+            //{
+            //    Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}");
+            //}
+
+            #endregion
+
+
+            #region problem 4
+
+            //var ordersPerStore = _applicationDb.Orders
+            //    .GroupBy(o => o.StoreId)
+            //    .Select(g => new
+            //    {
+            //        StoreId = g.Key,
+            //        OrderCount = g.Count()
+            //    });
+
+            //foreach (var store in ordersPerStore)
+            //    {
+            //    Console.WriteLine($"Store ID: {store.StoreId}, Number of Orders: {store.OrderCount}");
+            //}
+
+            #endregion
+
+            #region problem 5
+
+            //var orders = _applicationDb.Orders.Where(o => o.ShippedDate == null);
+
+            //foreach (var order in orders)
+            //{
+            //    Console.WriteLine($"Order ID: {order.OrderId}, Order Date: {order.OrderDate}, Customer ID: {order.CustomerId}, Staff ID: {order.StaffId}, Store ID: {order.StoreId}");
+            //}
+
+            #endregion
+
+
+            #region problem 6
+
+            //var customersWithOrders = _applicationDb.Customers.Select(c => new { 
+            //c.FirstName,
+            //c.LastName,
+            //count = c.Orders.Count()
+            //});
+
+
+            //foreach (var customer in customersWithOrders)
+            //{
+            //    Console.WriteLine($"Customer: {customer.FirstName} {customer.LastName}, Number of Orders: {customer.count}");
+            //}
+
+
+            #endregion
+
+
+            #region problem 7
+
+            //var products = _applicationDb.Products.Where(p => p.OrderItems.FirstOrDefault(o => p.ProductId == o.ProductId) == null);
+
+
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}");
+            //}
+
+            #endregion
+
+
+            #region problem 8
+
+
+            //var productsWithLowStock = _applicationDb.Products
+            //.Where(p => p.Stocks.Any(s => s.Quantity < 5));
+
+            //foreach (var product in productsWithLowStock)
+            //{
+            //    Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}");
+            //}
+
+            #endregion
+
+            #region problem 9
+
+            //var product = _applicationDb.Products.First();
+
+            //Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Price: {product.ListPrice}");
+
+            #endregion
+
+
+            #region problem 10
+
+            //var products = _applicationDb.Products.Where(p => p.ModelYear == 2016);
+
+
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Model Year: {product.ModelYear}");
+            //}
+
+            #endregion
+
+
+            #region problem 11
+
+
+            //var productsWithOrders = _applicationDb.Products.Select(p => new
+            //{
+            //    p.ProductId,
+            //    p.ProductName,
+            //    p.ModelYear,
+            //    count = p.OrderItems.Count(),
+            //});
+
+            //foreach (var product in productsWithOrders)
+            //{
+            //    Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Model Year: {product.ModelYear}, Number of Orders: {product.count}");
+            //}
+
+            #endregion
+
+
+            #region problem 12
+
+            //int products = _applicationDb.Products.Count(e => e.CategoryId == 3);
+
+
+            //Console.WriteLine($"Number of products in category 3: {products}");
+
+
+            #endregion
+
+            #region problem 13
+
+            //var avg = _applicationDb.Products.Average(a => a.ListPrice);
+
+            //Console.WriteLine($"The Avg is: {avg}");
+
+            #endregion
+
+
+            #region problem 14
+
+            //var product = _applicationDb.Products.Find(3);
+
+            //if (product != null)
+            //{
+
+            //  Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Price: {product.ListPrice}");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Product not found.");
+            //}
+
+            #endregion
+
+
+            #region problem 15
+
+            //var products = _applicationDb.Products.Where(p => p.OrderItems.Any(o => o.Quantity > 3));
+
+            //foreach (var p in products)
+            //{
+            //    Console.WriteLine($"Product ID: {p.ProductId}, Product Name: {p.ProductName}");
+            //}
+
+
+
+            #endregion
+
+
+            #region problem 16
+
+            //var staffs = _applicationDb.Staffs.Select(s => new
+            //{
+            //    s.FirstName,
+            //    s.LastName,
+            //    count = s.Orders.Count()
+            //});
+
+            //foreach (var staff in staffs)
+            //{
+            //    Console.WriteLine($"Staff: {staff.FirstName} {staff.LastName}, Number of Orders: {staff.count}");
+            //}
+
+
+            #endregion
+
+            #region problem 17
+
+
+            //var activeStaffs = _applicationDb.Staffs.Where(s => s.Active == 1);
+
+            //foreach (var staff in activeStaffs)
+            //{
+            //    Console.WriteLine($"Staff ID: {staff.StaffId}, Name: {staff.FirstName} {staff.LastName}, Active: {staff.Active}");
+            //}
+
+            #endregion
+
+
+            #region problem 18
+
+            //var products = _applicationDb.Products
+            //.Include(p => p.Category)
+            //.Include(p => p.Brand);
+
+            //foreach (var product in products)
+            //{
+            //    Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Category: {product.Category.CategoryName}, Brand: {product.Brand.BrandName}");
+            //}
+
+            #endregion
+
+
+            #region problem 19
+            //var orders = _applicationDb.Orders.Where(o => o.ShippedDate != null);
+
+            //foreach (var order in orders)
+            //{
+            //    Console.WriteLine($"Order ID: {order.OrderId}, Order Date: {order.OrderDate}, Customer ID: {order.CustomerId}, Staff ID: {order.StaffId}, Store ID: {order.StoreId}");
+            //}
+
+            #endregion
+
+
+            #region problem 20
+
+
+
+            var products = _applicationDb.Products
+            .Select(p => new
+            {
+                p.ProductId,
+                p.ProductName,
+                p.BrandId,
+                p.CategoryId,
+                p.ModelYear,
+                p.ListPrice,
+                TotalQuantityOrdered = p.OrderItems.Sum(oi => oi.Quantity)
+            });
+
+            foreach (var product in products)
+            {
+                Console.WriteLine($"Product ID: {product.ProductId}, Product Name: {product.ProductName}, Total Quantity Ordered: {product.TotalQuantityOrdered}");
+            }
+
+            #endregion
+        }
+    }
+}
